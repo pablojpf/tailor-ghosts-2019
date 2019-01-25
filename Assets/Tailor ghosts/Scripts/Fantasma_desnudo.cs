523d270f1414bd83d256499b3cb994e8bf6068c9
LFallenAngel <45042273+LFallenAngel@users.noreply.github.com>
2019-01-25 20:19:37 +0100

    #10 Programar mecánicas fantasma desnudo
    Para poder unirse a otro fantasma, primero debe pasar sobre la tela roja que estará en una casilla distinta. Así se convierte en un fantasma normal.

---
+++ Assets/Tailor ghosts/Scripts/Fantasma_desnudo.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fantasma_desnudo : MonoBehaviour
+{
+    /*La diferencia entre este script y el normal son las que 
+     * comentaré a continuacion
+     */
+
+
+    //Creamos dos vectores, uno para detectar dónde pulso por primera vez
+    //y dónde suelto para más tarde calcular la diferencia entre esas posiciones
+    new Vector3 pincho;
+    new Vector3 suelto;
+
+    
+    //Acceder al fantasma normal desde unity
+    public GameObject fantasmaNormal;
+
+
+    //Declaramos una velocidad 
+    //Y un booleano para saber si podemos movernos o no para así impedir
+    //que el jugador pueda mover el fantasma mientras este realiza su trayectoria
+    public float velocidad = 10f;
+    public bool puedoMoverme = true;
+    Rigidbody2D rb;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+  
+    void Update()
+    {
+      
+    }
+    void Mover()
+    {
+        //Declaramos un vector que obtenga la diferencia 
+        //Entre la posición donde pinchamos y dónde soltamos
+        //para saber la dirección en la que irá nuestro personaje
+
+        Vector3 dif = suelto - pincho;
+
+        if (puedoMoverme)
+        {
+
+            //Si el valor absoluto en x es mayor que el valor absoluto en y
+            //Entonces la velocidad irá en el eje x
+            //dependiendo de si la dirección es positiva o negativa irá hacia arriba o hacia abajo
+            if (Mathf.Abs(dif.x) > Mathf.Abs(dif.y))
+            {
+                if (dif.x > 0)
+                {
+                    rb.velocity = new Vector2(1, 0) * velocidad;
+                }
+                else
+                {
+                    rb.velocity = new Vector2(-1, 0) * velocidad;
+                }
+
+                puedoMoverme = false;
+            }
+            //Si no, la velocidad irá en el eje y
+            //dependiendo de si la dirección es positiva o negativa irá hacia derecha o izquierda
+            else
+            {
+                if (dif.y > 0)
+                {
+                    rb.velocity = new Vector2(0, 1) * velocidad;
+                }
+                else
+                {
+                    rb.velocity = new Vector2(0, -1) * velocidad;
+                }
+
+                puedoMoverme = false;
+            }
+
+        }
+
+
+
+
+    }
+
+    //Cuando pincho obtengo la posición donde he pinchado y la guardo en el vector pincho
+    void OnMouseDown()
+    {
+        pincho = Input.mousePosition;
+    }
+    //Cuando suelto obtengo la posición donde he soltado y la guardo en el vector suelto
+    void OnMouseUp()
+    {
+        suelto = Input.mousePosition;
+        Mover();
+
+    }
+
+    //Cuando choco con un colider puedo volver a moverme
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        puedoMoverme = true;
+
+        //Si el objeto con el que choco tiene el Tag TELA entonces me convierto en un
+        //fantasma de tipo normal, lo instanciamos y destruimos el desnudo
+        if (col.gameObject.CompareTag("Tela"))
+        {
+            Instantiate(fantasmaNormal, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+    }
+}
