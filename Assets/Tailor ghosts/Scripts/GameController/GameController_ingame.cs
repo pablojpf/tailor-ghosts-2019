0dcea532abaf7e1c2da3404e227f13b4a79c30c6
LFallenAngel <45042273+LFallenAngel@users.noreply.github.com>
2019-02-04 20:58:59 +0100

    #72 Crear menu juego ingame (sin acabar)

---
+++ Assets/Tailor ghosts/Scripts/GameController/GameController_ingame.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameController_ingame : MonoBehaviour
+{
+
+    public Animator anim_UIingame;
+    public Animator anim_pausa;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Pausa()
+    {
+      
+        anim_UIingame.SetBool("activar",true);
+        anim_pausa.SetBool("activar",true);
+        Time.timeScale = 0f;
+    }
+    public void Play()
+    {
+        anim_UIingame.SetBool("activar",false);
+        anim_pausa.SetBool("activar", false);
+        Time.timeScale = 1f;
+    }
+
+    public void ReiniciarNivel()
+    {
+        anim_pausa.SetBool("activar", false);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1f;
+    }
+    public void VolverMenu()
+    {
+        SceneManager.LoadScene(sceneName: "Menu_seleccion_nivel");
+        Time.timeScale = 1f;
+    }
+}
