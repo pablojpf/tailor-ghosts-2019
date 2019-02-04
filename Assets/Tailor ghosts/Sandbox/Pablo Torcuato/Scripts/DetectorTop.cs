5a2d380bf6b6cc3b46e30370199e41caf2c2ae12
Exater5 <45042365+Exater5@users.noreply.github.com>
2019-02-04 21:02:15 +0100

    Interaccion Caja

---
+++ Assets/Tailor ghosts/Sandbox/Pablo Torcuato/Scripts/DetectorTop.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectorTop : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        InteracCaja.top = true;
+        Invoke("Retorna", 0.3f);
+    }
+    public void Retorna()
+    {
+        InteracCaja.top = false;
+    }
+}
