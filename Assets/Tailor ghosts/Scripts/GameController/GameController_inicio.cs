dc5264b94f9397f02f7518e2468adba919bd4df8
LFallenAngel <45042273+LFallenAngel@users.noreply.github.com>
2019-02-06 20:35:41 +0100

    #141 | Hacer sonar botones menu inicial

--- Assets/Tailor ghosts/Scripts/GameController/GameController_inicio.cs
+++ Assets/Tailor ghosts/Scripts/GameController/GameController_inicio.cs
@@ -9,13 +9,26 @@ public class GameController_inicio : MonoBehaviour
 {
     public Animator anim_UIajustes;
     public Animator anim_MenuLogros;
+    public Animator anim_MusicaOnOff;
+    public Animator anim_SonidoOnOff;
     public GameObject botonPlay;
+
+
+    public AudioSource sonido_play;
+    public AudioSource sonido_ajustes;
+    public AudioSource sonido_atras;
+
+    private bool sonidoActivo = true;
+    private bool musicaActiva = true;
+
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+       
+       
+
     }
 
     // Update is called once per frame
@@ -26,6 +39,9 @@ public class GameController_inicio : MonoBehaviour
 
     public void Ajustes()
     {
+        sonido_ajustes.Play(0);
+
+        Debug.Log("ajustes");
         anim_UIajustes.SetBool("activar", true);
         anim_MenuLogros.SetBool("activar", false);
     }
@@ -37,5 +53,34 @@ public class GameController_inicio : MonoBehaviour
         anim_MenuLogros.SetBool("activar", true);
         
     }
+    public void SonidoAdelante()
+    {
+        sonido_play.Play(0);
+        Debug.Log("suena");
+    }
+    public void SonidoAtras()
+    {
+        sonido_atras.Play(0);
+        Debug.Log("suena");
+    }
+
+
+
+
+    
+    public void Sonidoonoff()
+    {
+
+        sonidoActivo = !sonidoActivo;
+        anim_SonidoOnOff.SetBool("activar", sonidoActivo);
+    }
+
+    public void Musicaonoff()
+    {
+        musicaActiva = !musicaActiva;
+        anim_MusicaOnOff.SetBool("activar", musicaActiva);
+     
+    }
+
 
 }
