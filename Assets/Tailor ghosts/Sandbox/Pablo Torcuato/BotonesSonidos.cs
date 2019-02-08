7a246d185c335f773bb6f0f92064f3763adfca58
Exater5 <pablotorcuato@gmail.com>
2019-02-08 20:42:38 +0100

    Zona de muerte Acabada

---
+++ Assets/Tailor ghosts/Sandbox/Pablo Torcuato/BotonesSonidos.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BotonesSonidos : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Hacia Delante Zir y enter maquina para play
+    //Hacia Atras Tijeras
+    //Solo un boton tiene audiosource
+}
