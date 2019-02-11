26888d902508590475c39d55cd2b0a02d1854bd7
LFallenAngel <45042273+LFallenAngel@users.noreply.github.com>
2019-02-11 13:15:52 +0100

    #146 Animacion estrellas cuando los fantasmas las ocupan

--- Assets/Tailor ghosts/Scripts/Estrellas/DetectorEstrella.cs
+++ Assets/Tailor ghosts/Scripts/Estrellas/DetectorEstrella.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class DetectorEstrella : MonoBehaviour
 {
     bool estrellaActiva = false;
-    
+    Animator anim_estrellas;
+
     // Start is called before the first frame update
     void Start()
     {
         GameController_ingame.estrellas = 0;
-
+        anim_estrellas = GetComponent<Animator>();
+        anim_estrellas.SetBool("activar", false);
     }
 
     // Update is called once per frame
@@ -23,8 +25,9 @@ public class DetectorEstrella : MonoBehaviour
         if (col.gameObject.CompareTag("Player") && !estrellaActiva)
         {
             GameController_ingame.estrellas++;
-          
-            estrellaActiva = true;
+            Debug.Log("he entrado");
+            
+            anim_estrellas.SetBool("activar", true);
         }
     }
     private void OnTriggerExit2D(Collider2D col)
@@ -33,6 +36,8 @@ public class DetectorEstrella : MonoBehaviour
         {
             estrellaActiva = false;
             GameController_ingame.estrellas--;
+
+            anim_estrellas.SetBool("activar", false);
         }
     }
 }
