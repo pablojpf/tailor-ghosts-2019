b5f0931f73539a79a80970aaee3082fb241b5c54
LFallenAngel <45042273+LFallenAngel@users.noreply.github.com>
2019-02-12 19:25:30 +0100

    #75 Musica mundo 2 + eliminar 3er nivel de la demo modifical menú

--- Assets/Tailor ghosts/Scripts/GameController/GameController_inicio.cs
+++ Assets/Tailor ghosts/Scripts/GameController/GameController_inicio.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-using UnityEngine.Audio
+using UnityEngine.Audio;
 
 
 public class GameController_inicio : MonoBehaviour
