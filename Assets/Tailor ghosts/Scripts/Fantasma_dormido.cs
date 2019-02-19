50c51b88ff857797291f6a7758bdf8344efb4765
LFallenAngel <45042273+LFallenAngel@users.noreply.github.com>
2019-02-19 20:36:17 +0100

    #160 Solventar bugs fantasma dormido

--- Assets/Tailor ghosts/Scripts/Fantasma_dormido.cs
+++ Assets/Tailor ghosts/Scripts/Fantasma_dormido.cs
@@ -4,18 +4,28 @@ using UnityEngine;
 
 public class Fantasma_dormido : MonoBehaviour
 {
-    
+
+    public GameObject fantasmaNormal;
+    public GameObject gc;
+
     Fantasma_dormido scriptFantasmaDormido;
-    public AudioController_InGame scriptACUnion;
+    //public AudioController_InGame scriptACUnion;
 
     private void OnCollisionEnter2D(Collision2D col)
     {
         
         if (col.gameObject.CompareTag("Player"))
         {
-            scriptACUnion.AudioUnion();
-            transform.SetParent(col.transform);           
-            Destroy(scriptFantasmaDormido);
+            GameObject fnuevoNormal;
+            Debug.Log("choco"+ col.transform.name);
+           // scriptACUnion.AudioUnion();
+
+            fnuevoNormal= Instantiate(fantasmaNormal, transform.position, transform.rotation);
+            fnuevoNormal.transform.SetParent(col.transform);
+            Destroy(fnuevoNormal.GetComponent<Rigidbody2D>());
+            Destroy(fnuevoNormal.GetComponent<Fantasma_normal>());
+            gc.GetComponent<GameController_ingame>().RestarFantasmas();
+            Destroy(gameObject);
         }
     }
 
