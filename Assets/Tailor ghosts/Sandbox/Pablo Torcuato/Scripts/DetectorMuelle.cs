6867e6373c63168e0dbd8a7a877bb5feb305e8d9
Exater5 <pablotorcuato@gmail.com>
2019-02-27 19:47:32 +0100

    Muelle_Basico

---
+++ Assets/Tailor ghosts/Sandbox/Pablo Torcuato/Scripts/DetectorMuelle.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectorMuelle : MonoBehaviour
+{
+    public bool top = false;
+    public bool bot = false;
+    public bool right = false;
+    public bool left = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+
+        {
+            if (top == true)
+            {
+                Muelle.top = true;
+            }
+            if (bot == true)
+            {
+                Muelle.bot = true;
+            }
+            if (right == true)
+            {
+                Muelle.right = true;
+            }
+            if (left == true)
+            {
+                Muelle.left = true;
+            }
+        }
+    }
+}
---
+++ Assets/Tailor ghosts/Sandbox/Pablo Torcuato/Scripts/Muelle.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Muelle : MonoBehaviour
+{
+    public static bool top = false;
+    public static bool bot = false;
+    public static bool left = false;
+    public static bool right = false;
+
+    public GameObject fantasma;
+
+    public GameObject detectadoTop;
+    public GameObject detectadoBot;
+    public GameObject detectadoRight;
+    public GameObject detectadoLeft;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Player")&& top == true)
+        {
+            col.transform.position = new Vector2(col.transform.position.x, col.transform.position.y + 1);
+            top = false;
+        }
+        if (col.gameObject.CompareTag("Player") && bot == true)
+        {
+            col.transform.position = new Vector2(col.transform.position.x, col.transform.position.y - 1);
+            bot = false;
+        }
+        if (col.gameObject.CompareTag("Player") && right == true)
+        {
+            col.transform.position = new Vector2(col.transform.position.x + 1, col.transform.position.y);
+            right = false;
+        }
+        if (col.gameObject.CompareTag("Player") && left == true)
+        {
+            col.transform.position = new Vector2(col.transform.position.x - 1, col.transform.position.y);
+            left = false;
+        }
+    }
+}
