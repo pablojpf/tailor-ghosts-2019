f5e653aff294040b5605776e84c348048c4a4698
Exater5 <pablotorcuato@gmail.com>
2019-02-27 20:57:40 +0100

    Muelle_Mejorando

--- Assets/Tailor ghosts/Sandbox/Pablo Torcuato/Scripts/Muelle.cs
+++ Assets/Tailor ghosts/Sandbox/Pablo Torcuato/Scripts/Muelle.cs
@@ -9,44 +9,91 @@ public class Muelle : MonoBehaviour
     public static bool left = false;
     public static bool right = false;
 
-    public GameObject fantasma;
+    bool paRiba = false;
+    bool paBajo = false;
+    bool paDerecha = false;
+    bool paIzquierda = false;
 
-    public GameObject detectadoTop;
-    public GameObject detectadoBot;
-    public GameObject detectadoRight;
-    public GameObject detectadoLeft;
+    GameObject quienChoca;
+    Vector2 posInicial;
+
+    Vector2 posFinalTop;
+    Vector2 posFinalBot;
+    Vector2 posFinalRight;
+    Vector2 posFinalLeft;
     // Start is called before the first frame update
     void Start()
     {
-        
+   
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (paRiba == true)
+        {
+            quienChoca.transform.position = Vector2.MoveTowards(quienChoca.transform.position, posFinalTop, 10 * Time.deltaTime);
+            if(new Vector2(quienChoca.transform.position.x, quienChoca.transform.position.y) == posFinalTop)
+            {
+                paRiba = false;
+                top = false;
+            }
+        }
+        if (paBajo == true)
+        {
+            quienChoca.transform.position = Vector2.MoveTowards(quienChoca.transform.position, posFinalBot, 10 * Time.deltaTime);
+            if (new Vector2(quienChoca.transform.position.x, quienChoca.transform.position.y) == posFinalBot)
+            {
+                paBajo = false;
+                bot = false;
+            }
+        }
+        if (paDerecha == true)
+        {
+            quienChoca.transform.position = Vector2.MoveTowards(quienChoca.transform.position, posFinalRight, 10 * Time.deltaTime);
+            if (new Vector2(quienChoca.transform.position.x, quienChoca.transform.position.y) == posFinalRight)
+            {
+                paDerecha = false;
+                right = false;
+            }
+        }
+        if (paIzquierda == true)
+        {
+            quienChoca.transform.position = Vector2.MoveTowards(quienChoca.transform.position, posFinalLeft, 100 * Time.deltaTime);
+            if (new Vector2(quienChoca.transform.position.x, quienChoca.transform.position.y) == posFinalLeft)
+            {
+                paIzquierda = false;
+                left = false;
+            }
+        }
     }
+
     private void OnCollisionEnter2D(Collision2D col)
     {
+
+        quienChoca = col.gameObject;
+        posInicial = quienChoca.transform.position;
+        
         if (col.gameObject.CompareTag("Player")&& top == true)
         {
-            col.transform.position = new Vector2(col.transform.position.x, col.transform.position.y + 1);
-            top = false;
+            posFinalTop = new Vector2(quienChoca.transform.position.x, Mathf.Round(quienChoca.transform.position.y + 1));
+            paRiba = true;
         }
         if (col.gameObject.CompareTag("Player") && bot == true)
         {
-            col.transform.position = new Vector2(col.transform.position.x, col.transform.position.y - 1);
-            bot = false;
+            posFinalBot = new Vector2(quienChoca.transform.position.x, Mathf.Round(quienChoca.transform.position.y - 1));
+            paBajo = true;
         }
         if (col.gameObject.CompareTag("Player") && right == true)
         {
-            col.transform.position = new Vector2(col.transform.position.x + 1, col.transform.position.y);
-            right = false;
+            posFinalRight = new Vector2(Mathf.Round(quienChoca.transform.position.x + 1), quienChoca.transform.position.y);
+            paDerecha = true;
         }
         if (col.gameObject.CompareTag("Player") && left == true)
         {
-            col.transform.position = new Vector2(col.transform.position.x - 1, col.transform.position.y);
-            left = false;
+            posFinalLeft = new Vector2(Mathf.Round(quienChoca.transform.position.x - 1), quienChoca.transform.position.y);
+            paIzquierda = true;
         }
     }
+
 }
