ae7eaeccd75de8774a8d03f941ffaee71939d6aa
Exater5 <pablotorcuato@gmail.com>
2019-03-05 14:47:51 +0100

    #178 Termino de comentar Costurera

--- Assets/Tailor ghosts/Sandbox/Pablo Torcuato/Scripts/Iman.cs
+++ Assets/Tailor ghosts/Sandbox/Pablo Torcuato/Scripts/Iman.cs
@@ -39,20 +39,25 @@ public class Iman : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Cada frame, muestra un rayo de color rojo para demostrar que NO esta detectando nada
         Debug.DrawRay(transform.position + new Vector3(0f, altura / 10, 0f), transform.right * distancia, Color.red);
+        //Aqui lanzamos un raycast de verdad de la misma distancia que el rojo mostrado antes
         hitRight = Physics2D.Raycast(transform.position + new Vector3 (0f,altura/10,0f), transform.right, distancia, capas);
         if (hitRight)
         {
+            //Si detecta una colisión, comprueba si el que detecta tiene el tag de player
             if (hitRight.transform.CompareTag("Player"))
             {
                 
                 
-
+                //Detiene al objeto que detecta 
                 hitRight.rigidbody.velocity = Vector2.zero;
+                //Muestra un rayo verde en el editor
                 Debug.DrawRay(transform.position, transform.right * distancia, Color.green);
+                //Establece 2 vectores, uno de la posicion de la costurera y otro del player que detecta su imán para hacer un MoveTowards
                 Vector2 miPosicion = new Vector2(transform.position.x +0.91f, hitRight.transform.position.y);
                 Vector2 posicionDelNormal = new Vector2(hitRight.transform.position.x, hitRight.transform.position.y);
-               
+                //Une al jugador con la costurera a traves de un MoveTowards
                 hitRight.transform.position = Vector2.MoveTowards(posicionDelNormal, miPosicion, velocidad * Time.deltaTime);
             }
           
@@ -60,16 +65,25 @@ public class Iman : MonoBehaviour
     }
     private void OnCollisionEnter2D(Collision2D col)
     {
+        //Esto está, para cuando termine de atraer al player y choque con la costurera, que ejecute lo siguiente:
         if (col.gameObject.CompareTag("Player"))
         {
+            //Activa el sonido de la union
             scriptACUnion.AudioUnion();
+            //Si el padre del transform del objeto con el que choco es distinto al mio...
             if (col.transform.parent != transform)
             {
+                //Creamos una variable de gameobject para mas tarde interactuar directamente con ella
                 GameObject copiaNormal;
+                //Hacemos una copia del fantasma normal
                 copiaNormal = Instantiate(fantasmaNormal, transform.position, transform.rotation);
+                //Convertimos la copia en hijo del Player con el que inicialmente hemos chocado
                 copiaNormal.transform.SetParent(col.transform);
+                //Obtenemos el rigidbody del fantasma normal y lo desactivamos
                 copiaNormal.GetComponent<Rigidbody2D>().simulated = false;
+                //Restamos un fantasma al gamecontroller para poder terminar el nivel
                 gc.GetComponent<GameController_ingame>().RestarFantasmas();
+                //Destruimos a la costurera
                 Destroy(gameObject);
                 //Si no desactivamos el rigidbody del fantasma al que nos unimos, solo se moverá 1
             }
